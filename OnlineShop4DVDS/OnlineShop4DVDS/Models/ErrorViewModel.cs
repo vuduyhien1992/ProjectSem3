e902bb3ffe6aae4f55c8fa3ce2046ab519e21190
websitepro100 <143474813+websitepro100@users.noreply.github.com>
2023-12-01 18:29:26 +0700

    Let go

---
+++ OnlineShop4DVDS/OnlineShop4DVDS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineShop4DVDS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
